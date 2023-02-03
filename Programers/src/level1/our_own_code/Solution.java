edf4e460c8ef2c4ba4adc9f6c5ebedfac5c80c2e
Choi-jun-ho <wnsgh7458969@gmail.com>
2023-02-03 16:45:42 +0900

    Feat: Solve the Programmers Lv1 '둘만의 암호'

---
+++ Programers/src/level1/our_own_code/Solution.java
@@ -0,0 +1,29 @@
+package level1.our_own_code;
+
+public class Solution {
+
+    public char addChar(char c) {
+        return (++c > 'z') ? 'a' : c;
+    }
+
+    public String solution(String s, String skip, int index) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (!skip.contains(String.valueOf(c))) {
+                for (int i = 0; i < index; i++) {
+                    c = addChar(c);
+                    while (skip.contains(String.valueOf(c)))
+                        c = addChar(c);
+                }
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("aukks",	"wbqd", 5));
+    }
+}
